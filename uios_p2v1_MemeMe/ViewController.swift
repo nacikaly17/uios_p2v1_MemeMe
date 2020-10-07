5da6b8de5d229471fb6c5d80fe3fdba9cb3684f7
Naci Kalyoncu <naci.kalyoncu@gmail.com>
2020-10-07 10:12:04 +0200

    Initial Submit

---
+++ uios_p2v1_MemeMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uios_p2v1_MemeMe
+//
+//  Created by Naci Kalyoncu on 07.10.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
